2147a373f2f245d21b1cae4b2d9308804ef2e937
Bohdan Mukha <by.muha@gmail.com>
2023-02-19 00:48:19 +0200

    exercise 4.5 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,10 @@ const dummy = () => 1
 
 const totalLikes = blogs => blogs.reduce((acc, curr) => acc+=curr.likes, 0)
 
+const favoriteBlog = blogs => blogs.reduce((acc, curr) => curr.likes > acc.likes ? curr : acc)
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
